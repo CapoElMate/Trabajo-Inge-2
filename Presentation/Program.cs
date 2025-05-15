d07ec91e8d41354d98155e1d76c340da51bbfe0f
CapoElMate <mateotroll460@gmail.com>
2025-05-14 22:06:57 -0300

    .

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -9,7 +9,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var provider = builder.Services.BuildServiceProvider();
+var dbContext = provider.GetRequiredService<ApplicationDbContext>();
+
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    // Agrega encabezados CORS directamente a cada respuesta
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+
+    // Manejo especial para solicitudes OPTIONS (preflight)
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.CompleteAsync();
+        return;
+    }
+
+    await next();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
