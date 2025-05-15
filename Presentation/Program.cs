795efec6693d74c05b1cfecfdf712183dd392882
Valen <valentin02antonini@gmail.com>
2025-05-14 21:08:29 -0300

    feat: add program.cs swagger endpoins and http config.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,23 +1,35 @@
+using System;
 using Data_Access_Layer;
 using Microsoft.EntityFrameworkCore;
 
+//localhost:5000/swagger
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5000); // ESCUCHA EN EL PUERTO 5000 SOLO HTTP.
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using (var scope = app.Services.CreateScope())
+
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        //ACA SE INTERACTUA CON EL CONTEXT DE LA DB.
+    }
 }
 
-app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
