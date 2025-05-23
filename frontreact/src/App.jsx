7bf3b48652d5f934c5c8c67b5e78fb837baed14d
DerDAVO <marcos.matias180834@alumnos.info.unlp.edu.ar>
2025-05-22 23:13:22 -0300

    agregando vistas

--- frontreact/src/App.jsx
+++ frontreact/src/App.jsx
@@ -1,22 +1,23 @@
-import LoginReal from './components/LoginReal';
-import {Routes, Route} from 'react-router-dom';
-import Login from "./components/Login";
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 import HomePage from './components/HomePage';
-import DetallePublicacion from './components/DetallePublicacion';
+import { AuthProvider } from './AuthContext';
 import SignUp from './components/SignUp';
 import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
-function App(){
-  return(
-    <Routes>
-      <Route path='/LoginReal' element={<LoginReal/>}/>
-      <Route path='/HomePage' element={<HomePage/>}/>
-      <Route path='/Login' element={<Login/>}/>
-      <Route path='/SignUp' element={<SignUp/>}/>
-      <Route path='/ForgotPassword' element={<ForgotPassword/>}/>
-      <Route path='/DetallePublicacion/:id' element={<DetallePublicacion/>}/>
-      <Route path='/ResetPassword' element={<ResetPassword/>}/>
-    </Routes>
+import ValidateUser from './components/ValidateUser';
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path='/ValidateUser' element={<ValidateUser />}/> 
+      </Routes>
+    </AuthProvider>
   );
-}
-export default App; 
+};
+
+export default App;
