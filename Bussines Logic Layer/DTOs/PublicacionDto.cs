a73f0f19d8a6e64c9e5009857f71273df6b5fdaa
CapoElMate <mateotroll460@gmail.com>
2025-05-24 17:58:18 -0300

    CAMBIOS CHIQUITOS

---
+++ Bussines Logic Layer/DTOs/PublicacionDto.cs
@@ -0,0 +1,24 @@
+﻿using Domain_Layer.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines_Logic_Layer.DTOs
+{
+    public class PublicacionDto
+    {
+        public int idPublicacion { get; set; }
+        public string Status { get; set; } = null!;
+        public double PrecioPorDia { get; set; }
+        public string Descripcion { get; set; } = null!;        
+        public MaquinaDto Maquina { get; set; } = null!;
+        public ICollection<TagPublicacionDto> TagsPublicacion { get; set; } = new List<TagPublicacionDto>();
+        public ICollection<ComentarioDto> Comentarios { get; set; } = new List<Comentario>();        
+        public PoliticaDeCancelacionDto PoliticaDeCancelacion { get; set; } = null!;
+        public UbicacionDto Ubicacion { get; set; } = null!;
+            
+    }
+}
---
+++ Bussines Logic Layer/DTOs/TagPublicacionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines_Logic_Layer.DTOs
+{
+    class TagPublicacionDto
+    {
+        public string Tag { get; set; } = null!;
+    }
+}
