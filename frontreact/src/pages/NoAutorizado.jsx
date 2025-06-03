84fcea6cb6681613fca321f8d74feba082e179c9
DerDAVO <marcos.matias180834@alumnos.info.unlp.edu.ar>
2025-06-03 20:20:30 -0300

    agregando filtro a HomePage}

--- frontreact/src/pages/NoAutorizado.jsx
+++ frontreact/src/pages/NoAutorizado.jsx
@@ -1,13 +1,3 @@
-const NoAutorizado = () => {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>403 - No autorizado</h1>
-      <p>No tenés permiso para acceder a esta página.</p>
-    </div>
-  );
-};
-
-export default NoAutorizado;
 
 // NoAutorizado.jsx
 import React from 'react';
